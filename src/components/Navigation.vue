dd52e903b15ae36b640646bce61cb1df2aecc4aa
Andrew Schieltz <drewschieltz@gmail.com>
2022-09-26 17:50:33 -0400

    Cleaned up the navigation bar appearance

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,10 @@
 
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">John's House IOT</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <a class="navbar-brand mr-auto" href="/">John Maltinksy - IOT</a>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
@@ -17,8 +17,17 @@
 
 <style>
 
-.navbar {
-    padding-left: 50px;
+.navbar-toggler {
+    margin-left: 5px;
+}
+
+.navbar-brand {
+    margin-left: 10px;
+}
+
+.navbar-nav {
+    margin-left: 20px;
+    margin-right: 20px;
 }
 
 </style>
